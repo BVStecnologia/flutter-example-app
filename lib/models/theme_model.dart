521c85bba5b041162672148a104a585a9d8fc93f
Valdair <valdair3d@gmail.com>
2025-04-01 17:30:19 -0300

    Adiciona modelo para controle de tema

---
+++ lib/models/theme_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ThemeModel with ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeMode get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
